39d7ba731306c1b0fcf96f6242689e710454f049
Ryan Grove <ryan@wonko.com>
2012-07-27 12:42:34 -0700

    OMG KITTENS!

---
+++ web.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+    kittens = require('./lib/kittens');
+
+var app = express();
+
+app.engine('handlebars', require('./lib/handlebars-engine'));
+
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.render('index', {
+        kittenHTML: kittens.randomKittenHTML()
+    });
+});
+
+var port = process.env.PORT || 5000;
+
+app.listen(port, function () {
+    console.log('Listening on port ' + port);
+});
