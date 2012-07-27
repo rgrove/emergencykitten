0a2c226b9c0797d8873eafbe8749bc23966186db
Ryan Grove <ryan@wonko.com>
2012-07-27 16:57:31 -0700

    Add permalinks and a 404 kitten.

--- web.js
+++ web.js
@@ -16,6 +16,43 @@ app.get('/', function (req, res) {
     });
 });
 
+app.get('/img/random', function (req, res) {
+    res.redirect(302, kittens.randomKitten().imageUrl);
+});
+
+app.get('/img/:id', function (req, res, next) {
+    var kitten = kittens.byId(req.params.id);
+
+    if (!kitten) {
+        return next();
+    }
+
+    res.redirect(302, kitten.imageUrl);
+});
+
+app.get('/kitten/:id', function (req, res, next) {
+    var kitten = kittens.byId(req.params.id);
+
+    if (!kitten) {
+        return next();
+    }
+
+    res.render('index', {
+        kitten: kitten
+    });
+});
+
+// 404 handler (must come last)
+app.get('*', function (req, res) {
+    res.set('Status', 404);
+
+    res.render('404', {
+        kitten: kittens.randomKitten(),
+        path  : decodeURIComponent(req.path),
+        title : '404 Oh Noes!'
+    });
+});
+
 var port = process.env.PORT || 5000;
 
 app.listen(port, function () {
