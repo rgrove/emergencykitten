92d913cedc0b94018094e813e0ffd0bc418026db
Ryan Grove <ryan@wonko.com>
2022-12-10 18:00:18 -0800

    ✨ Sometimes fetch a random kitten from Flickr
    Since the curated list of kittens is pretty small, frequent visitors are
    likely to run out of fresh kittens.
    
    To prevent this, there’s now a random chance that a kitten will be
    fetched from the Flickr API instead of the curated list.
    
    Flickr’s search API generally returns pretty great kitten photos, but
    every now and then something will sneak in that’s not actually a kitten.
    Not much we can do about that, but you can just reload if that happens.

--- kittens.js
+++ kittens.js
@@ -408,3 +408,79 @@ const kittens = [
     "imageAlt": "A black and white kitten with a googly eye on their forehead."
   }
 ];
+
+/**
+ * Returns data for a random kitten photo.
+ *
+ * There's a random chance this will be either a curated photo or a random
+ * Flickr photo. Flickr photos are a little risky since we can't guarantee
+ * they're actually photos of kittens (or that they're even good photos), but
+ * including them in the mix ensures an endless supply of kittens.
+ */
+async function getRandomKitten() {
+  let randomKitten = kittens[Math.floor(Math.random() * kittens.length)];
+
+  if (Math.random() < 0.5) {
+    return randomKitten;
+  }
+
+  let flickrPhotos = await fetchFlickrPhotos();
+
+  if (flickrPhotos.length > 0) {
+    let attempts = 0;
+    let photo;
+
+    // Some Flickr photos don't have a `url_l` size available. If this happens,
+    // try up to 25 times to find a photo that'll work before giving up.
+    do {
+      attempts += 1;
+      photo = flickrPhotos[Math.floor(Math.random() * flickrPhotos.length)];
+    } while (!photo.url_l && attempts < 25)
+
+    if (photo.url_l) {
+      return {
+        imageAlt: photo.title,
+        imageUrl: photo.url_l,
+        url: `https://www.flickr.com/photos/${photo.owner}/${photo.id}`,
+      };
+    }
+  }
+
+  return randomKitten;
+}
+
+async function fetchFlickrPhotos() {
+  let url = new URL('https://www.flickr.com/services/rest');
+
+  // See https://www.flickr.com/services/api/flickr.photos.search.html
+  url.search = new URLSearchParams({
+    method: 'flickr.photos.search',
+    api_key: 'c1e552ce2698888886d28bd83d717d4c',
+    content_type: '1', // photos only
+    content_types: '0', // non-virtual photos
+    extras: 'url_l',
+    format: 'json',
+    license: '1,2,3,4,5,6,7,8,9,10', // CC licensed or public domain
+    media: 'photos',
+    nojsoncallback: '1',
+    per_page: '25',
+    sort: 'interestingness-desc', // tends to produce good photos, and each search seems to return a different set
+    tags: 'kitten'
+  }).toString();
+
+  let response = await fetch(url);
+
+  if (!response.ok) {
+    console.error('Flickr API request failed', response.status, response.statusText);
+    return [];
+  }
+
+  try {
+    let { photos } = await response.json();
+    return photos.photo ?? [];
+  } catch (err) {
+    console.error(err);
+  }
+
+  return [];
+}
